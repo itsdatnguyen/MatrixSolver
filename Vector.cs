9f80a4f707bc5c20f145a745e160df49e9447747
Dat Nguyen <itsdatnguyen@gmail.com>
2023-07-10 16:58:57 -0400

    ,

--- Vector.cs
+++ Vector.cs
@@ -19,6 +19,16 @@ public class Vector : IEnumerable<double>
         Values = values.ToArray();
     }
 
+    public double DotProduct(Vector rhs)
+    {
+        var sum = 0D;
+        for (var i = 0; i < Values.Length; i++)
+        {
+            sum = sum + Values[i] * rhs.Values[i];
+        }
+        return sum;
+    }
+
     public static Vector operator *(Vector lhs, double rhs) => new Vector(lhs.Select(v => v * rhs));
     public static Vector operator *(double lhs, Vector rhs) => rhs * lhs;
 
@@ -43,6 +53,21 @@ public class Vector : IEnumerable<double>
             outputValues[i] = lhs.Values[i] - rhs.Values[i];
         }
         return new Vector(outputValues);
+    }
+
+    public static Vector operator *(Vector lhs, RectangularMatrix rhs)
+    {
+        var values = new double[lhs.Values.Length];
+        for (var row = 0; row < rhs.Rows; row++)
+        {
+            var total = 0D;
+            for (var col = 0; col < lhs.Values.Length; col++)
+            {
+                total += lhs.Values[col] * rhs[col, row];
+            }
+            values[row] = total;
+        }
+        return new Vector(values);
     }
 
     public IEnumerator<double> GetEnumerator() => Values.ToList().GetEnumerator();
